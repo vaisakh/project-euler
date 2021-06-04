bf96e3e19588d64ff5cc5ba9b71d1f9d8f9b5fa8
vaisakh <mail.vaisakhvm@gmail.com>
2021-06-04 10:30:40 +0530

    completes problem 01 with sequence generator

--- euler-01.js
+++ euler-01.js
@@ -1,5 +1,11 @@
+
+function* generateNumbers (start, end) {
+	for(let i = start; i < end; i++) yield i;
+}
+
+const numbers = [...generateNumbers(1, 1000)];
 const filterBy = num => num % 3 === 0 || num % 5 === 0;
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
-const x = [1,2,3,4,5,6,7,8,9].filter(i => filterBy(i)).reduce(reducer)
+const x = numbers.filter(i => filterBy(i)).reduce(reducer)
 
 console.log(x);
